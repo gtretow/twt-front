ec92af51100b17b7a2225dee781928fef311576c
gtretow <guilherme.tretow@gmail.com>
2021-03-10 01:57:04 -0300

    tweet component

--- src/components/MainLayout/Tweet/Tweet.js
+++ src/components/MainLayout/Tweet/Tweet.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Container,
+  Body,
+  Avatar,
+  Content,
+  Header,
+  Dot,
+  Description,
+  ImageContent,
+  Icons,
+  Status,
+  CommentIcon,
+  RetweetIcon,
+  LikeIcon,
+} from "./index";
 
 function Tweet() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Container>
+      <Body>
+        <Avatar />
+        <Content>
+          <Header>
+            <strong>Provi</strong>
+            <span>@provi</span>
+            <Dot />
+            <time>10 de fev</time>
+          </Header>
+          <Description>Testetesteteste</Description>
+          <ImageContent />
+          <Icons>
+            <Status>
+              <CommentIcon />
+              20
+            </Status>
+            <Status>
+              <RetweetIcon />
+              2000
+            </Status>
+            <Status>
+              <LikeIcon />
+              20000
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
 }
 
-export default Tweet
+export default Tweet;
--- src/styles/icons.js
+++ src/styles/icons.js
@@ -6,10 +6,9 @@ export {
   ExitToApp,
   LocationOn,
   Search,
+  Replay,
 } from "styled-icons/material-outlined";
 
 export { Person, Cake, Favorite } from "styled-icons/material";
 export { ArrowLeft } from "styled-icons/heroicons-solid";
 export { Chat } from "styled-icons/heroicons-outline";
-
-
